dbdde27aee5997e1ff99819c474d4d14ee05370a
yaohaoou <yhogz001092@163.com>
2021-09-10 15:33:02 +0800

    git_test

---
+++ test/__init__.py
@@ -0,0 +1,12 @@
+#！/usr/local/bin/python3
+# _*_ coding:utf-8 _*_
+
+"""
+@Time     :  2021/9/10 15:30
+@Author   :  Justin Yao
+@Email    :  yaogz001092@163.com
+@Project  :  Git
+@File     :  __init__.py.py
+@Software :  PyCharm
+"""
+$END$
---
+++ test/hello_test.py
@@ -0,0 +1,13 @@
+#！/usr/local/bin/python3
+# _*_ coding:utf-8 _*_
+
+"""
+@Time     :  2021/9/10 15:31
+@Author   :  Justin Yao
+@Email    :  yaogz001092@163.com
+@Project  :  Git
+@File     :  hello_test.py
+@Software :  PyCharm
+"""
+print("hello world")
+
